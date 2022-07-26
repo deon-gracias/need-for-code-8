698f1cd31ba2c72549c39581617ecca797bb71ed
Aditya <vyas6238@gmail.com>
2022-07-27 00:57:01 +0530

    added cart models

---
+++ backend/models/cartModel.js
@@ -0,0 +1,36 @@
+// const mongoose = require('mongoose')
+const { Schema, model } = require('mongoose')
+// const ObjectID = mongoose.Schema.Types.ObjectId
+
+const cartSchema = new Schema({
+    user : {
+      type: Schema.Types.ObjectId,
+       required: true,
+       ref: 'User'
+     },
+    product: [{
+      itemId: {
+       type: Schema.Types.ObjectId,
+       ref: 'Product',
+       required: true
+    },
+
+    quantity: {
+       type: Number,
+       required: true,
+       min: 1,
+       default: 1},
+       price: Number
+     }],
+    total: {
+        type: Number,
+        required: true,
+       default: 0
+      }
+    }, {
+    timestamps: true
+    })
+
+const Cart = model('cart', cartSchema);
+module.exports = Cart;
+
