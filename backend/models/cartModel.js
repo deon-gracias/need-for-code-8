87490355a7e2bbc6d0e5973c8e372a83a90c4a7d
deon-gracias <deongracias1@gmail.com>
2022-07-27 07:50:43 +0530

    Changed item to product

--- backend/models/cartModel.js
+++ backend/models/cartModel.js
@@ -3,33 +3,34 @@ const { Schema, model } = require('mongoose')
 // const ObjectID = mongoose.Schema.Types.ObjectId
 
 const cartSchema = new Schema({
-    user : {
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  product: [{
+    productId: {
       type: Schema.Types.ObjectId,
-       required: true,
-       ref: 'User'
-     },
-    product: [{
-      itemId: {
-       type: Schema.Types.ObjectId,
-       ref: 'Product',
-       required: true
+      ref: 'Product',
+      required: true
     },
 
     quantity: {
-       type: Number,
-       required: true,
-       min: 1,
-       default: 1},
-       price: Number
-     }],
-    total: {
-        type: Number,
-        required: true,
-       default: 0
-      }
-    }, {
-    timestamps: true
-    })
+      type: Number,
+      required: true,
+      min: 1,
+      default: 1
+    },
+    price: Number
+  }],
+  total: {
+    type: Number,
+    required: true,
+    default: 0
+  }
+}, {
+  timestamps: true
+})
 
 const Cart = model('cart', cartSchema);
 module.exports = Cart;
